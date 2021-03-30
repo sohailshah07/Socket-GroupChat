e45eda082ec130f00aaa7c466c3b5978a002afc2
Sohail <sohail.mksn@gmail.com>
2021-03-30 15:36:34 +0530

    finishing app

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const socket = require('socket.io');
-
+const path = require('path');
 //app setup
 
 const PORT = 3000;
-
 const app = express();
 
 const server = app.listen(PORT, () =>{
@@ -13,12 +12,32 @@ const server = app.listen(PORT, () =>{
 })
 
 //static file
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
 //socket setup
 
 const io = socket(server);
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + './public/index.html');
+})
+
+var name;
+
 io.on("connection",function (socket)  {
-    console.log("made socket connection")
+    console.log("new user connected")
+    
+    socket.on('joining message',(userName) => {
+        name = userName;
+        io.emit('chat message',`---${name} joined the chat---`);
+    })
+
+    socket.on('disconnect',() => {
+        console.log("user disconnect")
+        io.emit('chat message',`---${name} left the chat---`);
+    })
+
+    socket.on('chat message',(msg)=> {
+        socket.broadcast.emit('chat message',msg)
+    });
 })
