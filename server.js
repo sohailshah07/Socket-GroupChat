15af788f88ad9b01f2bfa7b2bde08df43fccb524
Sohail <sohail.mksn@gmail.com>
2021-03-30 11:37:45 +0530

    server created

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const socket = require('socket.io');
+
+//app setup
+
+const PORT = 3000;
+
+const app = express();
+
+const server = app.listen(PORT, () =>{
+    console.log('listening on port '+PORT);
+    console.log('http://localhost:' + PORT);
+})
+
+//static file
+app.use(express.static("public"));
+
+//socket setup
+
+const io = socket(server);
+
+io.on("connection",(socket)=>{
+    console.log("made socket connection")
+})
